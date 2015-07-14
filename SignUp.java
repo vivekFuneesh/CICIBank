0fa639d164c3b992569ff6be5a8a4dce4ec9f214
Vivek Mangla <vivekFuneesh@users.noreply.github.com>
2015-07-14 14:28:00 +0530

    Update SignUp.java

--- SignUp.java
+++ SignUp.java
@@ -1,4 +1,26 @@
 
+/*
+ *@author: Vivek Mangla.
+ **/
+
+/*******************************************************************
+SignUp.java is part of PROJECT_NAME.
+
+    ProductCatalogue is free TYPE_OF_SOFTWARE: you can redistribute it    and/or modify
+    it under the terms of the GNU General Public License as published     by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    ProductCatalogue is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with ProductCatalogue.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************
+*/
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
